62e71b43c32d019d8c04ad03ca9e6cbe7c525bcc
StephanieRamirez <steponme547@gmail.com>
2017-09-22 20:16:05 -0700

    Added Mecanum Hardware and Teleop files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumHardware.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+/**
+ * Created by StephanieRamirez on 9/22/17.
+ */
+
+public class MecanumHardware {
+    public DcMotor frontLeftMotor = null;
+    public DcMotor frontRightMotor = null;
+    public DcMotor backLeftMotor = null;
+    public DcMotor backRightMotor = null;
+
+    private HardwareMap hwMap = null;
+
+    public MecanumHardware() {
+
+    }
+
+    public void init(HardwareMap ahwMap) {
+        hwMap = ahwMap;
+
+        // Define and Initialize Motors
+        frontLeftMotor = hwMap.dcMotor.get("frontLeftMotor");
+        frontRightMotor = hwMap.dcMotor.get("frontRightMotor");
+        backLeftMotor = hwMap.dcMotor.get("backLeftMotor");
+        backRightMotor = hwMap.dcMotor.get("backRightMotor");
+
+        // Set all motors to zero power
+        frontLeftMotor.setPower(0);
+        frontRightMotor.setPower(0);
+        backLeftMotor.setPower(0);
+        backRightMotor.setPower(0);
+
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+        frontLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+    }
+}
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleop.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/**
+ * Created by StephanieRamirez on 9/22/17.
+ */
+@TeleOp(name = "MecanumTeleop")
+public class MecanumTeleop extends LinearOpMode {
+
+    private MecanumHardware robot = new MecanumHardware();
+
+    @Override
+    public void runOpMode() {
+
+        robot.init(hardwareMap);
+
+        while (opModeIsActive()) {
+
+            double forward = -gamepad1.left_stick_y;
+            double strafeRight = gamepad1.left_stick_x;
+            double rotateRight = gamepad1.right_stick_x;
+
+            double frontLeftPower = forward + strafeRight + rotateRight;
+            double frontRightPower = forward - strafeRight - rotateRight;
+            double backLeftPower = forward - strafeRight + rotateRight;
+            double backRightPower = forward + strafeRight - rotateRight;
+
+            double max = Math.max(Math.abs(frontLeftPower), Math.max(Math.abs(frontRightPower), Math.max(Math.abs(backLeftPower), Math.abs(backRightPower))));
+
+            if (max > 1){
+                frontLeftPower /= max;
+                frontRightPower /= max;
+                backLeftPower /= max;
+                backRightPower /= max;
+            }
+
+            robot.frontLeftMotor.setPower(frontLeftPower);
+            robot.frontRightMotor.setPower(frontRightPower);
+            robot.backLeftMotor.setPower(backLeftPower);
+            robot.backRightMotor.setPower(backRightPower);
+        }
+
+
+    }
+}
